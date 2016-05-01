4675728918933127d6278e05e69216ea20ce7931
bitterypaul <bitterypaul@gmail.com>
2016-05-01 20:14:25 +0530

    Create uninstall.sh

---
+++ uninstall/uninstall.sh
@@ -0,0 +1,5 @@
+sudo bash
+apt-get remove --purge hostapd dnsmasq 
+
+rm -r /etc/pihole /etc/.pihole /opt/pihole /var/www /etc/dnsmasq.d /etc/dnsmasq.conf /etc/default/hostapd /etc/default/dnsmasq
+
